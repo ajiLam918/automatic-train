dc5764c62e2fad5692119e3035268f209f20cae9
ajiLam918 <167869006+ajiLam918@users.noreply.github.com>
2024-05-03 13:05:00 +0700

    Update se92n.sh

--- scripts/se92n.sh
+++ scripts/se92n.sh
@@ -12,7 +12,7 @@ curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 	&& apt update \
 	&& apt install ngrok -y
 ngrok config add-authtoken 2eTzTtisRPj4pnUlPB0p8kGKdMR_5QHLfQkLt8qeaTwFDRnRP
-ngrok http --region ap 5900 &>/dev/null &
+ngrok tcp --region ap 5900 &>/dev/null &
 qemu-img create -f qcow2 win8.img 128G
 qemu-system-x86_64 -hda win8.img -m 2G -boot d -cdrom "win.iso" -smp cores=2 -cpu host -enable-kvm -vnc :0 -machine usb=on -device usb-tablet &>/dev/null &
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
